12323591d7884322868478199684a206c2b6d7fa
rizqisamawa <rizqisamawa11@gmail.com>
2024-07-08 10:52:43 +0700

    chore: initialize

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-black font text-3xl">Hello Lingo!</p>
+      <Button size="lg" variant="destructive">
+        Click
+      </Button>
+    </div>
+  );
+}
